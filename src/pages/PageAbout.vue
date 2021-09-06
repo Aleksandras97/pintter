db1d56a18ad7426130792ca33585041b095eb16a
aleksandrsa97 <aleksandrnarusevic1@gmail.com>
2021-09-06 22:35:17 +0300

    Added About page

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="1-mt-none q-mb-md text-weight-bold">About Pintter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+        adipisci itaque nulla, repellendus ut ullam autem maxime incidunt nobis.
+        Blanditiis, eligendi cumque. Odit sequi dolorum reprehenderit quos, eum
+        illum eos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+        adipisci itaque nulla, repellendus ut ullam autem maxime incidunt nobis.
+        Blanditiis, eligendi cumque. Odit sequi dolorum reprehenderit quos, eum
+        illum eos.
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
